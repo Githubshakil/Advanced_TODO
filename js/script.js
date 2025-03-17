4881f3fffd2900eee26833b2a0335bad62fe593c
Githubshakil <shakilmahmud007@gmail.com>
2025-03-18 01:37:44 +0600

    update

--- js/script.js
+++ js/script.js
@@ -5,8 +5,6 @@ function $(id){
 const form = $('form')
 const date = $('date')
 const tbody = $('tbody')
-
-
 const today = new Date().toISOString().slice(0, 10);
 date.value = today;
 
@@ -18,46 +16,53 @@ date.value = today;
 
 form.addEventListener('submit', function(e){
 e.preventDefault()
-const inputElements = ([...this.elements]);
-
+const inputElements = [...this.elements];
 const formData = {}
 
 let isValid = true;
 inputElements.forEach(element => {
    if(element.type !== 'submit'){
-
-      if(element.value === ''){
-             alert('Please fill in all fields');
-             isValid = false;
-             return;
-      }
-       formData[element.name] = element.value
+    if(element.value == ''){
+        alert('Please fill all fields before submitting');
+        isValid = false;
+        return;
+    }
+     formData[element.name] = element.value;
    }
   
 })
 
 
-formData.status = 'incomplete';
-displayToUI(formData);
+if(isValid){
+    formData.status = 'incomplete';
+    displayToUI(formData);
+}
+
 this.reset()
 
 
 });
 
 
-function displayToUI({name, priority, status, date}){
+function displayToUI({
+    Name, 
+    Priority, 
+    status, 
+    date
+}){
     const tr = document.createElement('tr')
     tr.innerHTML = `
             <td>0</td>
-            <td>${name}</td>
-            <td>${priority}</td>
+            <td>${Name}</td>
+            <td>${Priority}</td>
             <td>${status}</td>
             <td>${date}</td>
             <td>
                 <button id="delete"><i class="fas fa-trash-can"></i></button>
                 <button id="check"><i class="fas fa-check-to-slot"></i></button>
                 <button id="edit"><i class="fas fa-pen-nib"></i></button>
-            </td>`
+            </td>
+            `
 
-    tbody.appendChild(tr)
-}
+            tbody.appendChild(tr)
+        }
